63a1f007dad198a4cc7436213c7195928be35a38
FireSuperior482 <sravi1360@gmail.com>
2024-09-27 20:39:49 +0530

    okay

--- Assets/ScriptableObjects/ItemScriptableObject.cs
+++ Assets/ScriptableObjects/ItemScriptableObject.cs
@@ -4,13 +4,7 @@ using System.Numerics;
 using UnityEngine;
 
 
-[System.Serializable]
-public struct Item
-{
-    public string name;
-    public int id;
-    public GameObject prefab;
-}
+
 
 [CreateAssetMenu]
 public class Items : ScriptableObject
--- Assets/Scripts/taskgenerator.cs
+++ Assets/Scripts/taskgenerator.cs
@@ -5,29 +5,42 @@ using TMPro;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
+[System.Serializable]
+public struct Item
+{
+    public string name;
+    public int id;
+    public GameObject prefab;
+}
+
 public class taskgenerator : MonoBehaviour
 {
     public static Dictionary<int,int> matchlist, currentlist;
-    [SerializeField] Items allitems;
+    [SerializeField] public List<Item> allitems;
     [SerializeField] GameObject textprefab;
     [SerializeField] GameObject textlayout;
-
     void Start()
     {
         matchlist = new Dictionary<int,int>();
         currentlist = new Dictionary<int,int>();
-        newlist(5);
+
+       
+        newlist(2);
         displaylist();
+        
     }
 
     void newlist(int n,int maxcount =1)
     {
+       
         matchlist.Clear();
         int i = 0;
         int temp;
+        print(allitems[0].name);
+
         while (i < n)
         {
-            temp = Random.Range(0, allitems.items.Count);
+            temp = Random.Range(0, allitems.Count);
             if (!matchlist.ContainsKey(temp))
             {
                 matchlist.Add(temp, 1);
@@ -44,7 +57,7 @@ public class taskgenerator : MonoBehaviour
         foreach (KeyValuePair<int, int> kvp in matchlist)
         {
             temp = Instantiate(textprefab, textlayout.transform);
-            temp.GetComponent<TMP_Text>().text = allitems.items[kvp.Key].name + "  " + kvp.Value.ToString();
+            temp.GetComponent<TMP_Text>().text = allitems[kvp.Key].name + "  " + kvp.Value.ToString();
         }
     }
 }
